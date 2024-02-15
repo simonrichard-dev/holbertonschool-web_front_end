2c01e434a3f342e4bd1aafb0dbf17f0a5be289df
simonrichard-dev <simon.insa@gmail.com>
2024-02-15 11:31:13 +0100

    suppress closure

--- Javascript_advanced/8-payments.js
+++ Javascript_advanced/8-payments.js
@@ -1,20 +1,15 @@
 function processPayment(amount) {
-    return function () {
         console.log('Collecting payment of ' + amount);
-    }
 }
 
 function processOrder(orderId, amount) {
-    return function() {
     console.log(orderId + ' is being processed');
-    let inner = processPayment(amount);
-    inner();
+    processPayment(amount);
     console.log(orderId + ' has been fully processed');
-    };
 }
 
 console.log("Processing orders");
-processOrder(1321, 10.99)();
-processOrder(1322, 12.99)();
-processOrder(1323, 15.0)();
+processOrder(1321, 10.99);
+processOrder(1322, 12.99);
+processOrder(1323, 15.0);
 console.log("All the orders have been processed");
