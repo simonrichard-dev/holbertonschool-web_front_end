f6649d7eea1225e3d9554a251dafc777f18062a5
simonrichard-dev <simon.insa@gmail.com>
2024-02-15 11:10:59 +0100

    add file for task 8. Stack order in functions

---
+++ Javascript_advanced/8-payments.js
@@ -0,0 +1,20 @@
+function processPayment(amount) {
+    return function () {
+        console.log('Collecting payment of ' + amount);
+    }
+}
+
+function processOrder(orderId, amount) {
+    return function() {
+    console.log(orderId + ' is being processed');
+    let inner = processPayment(amount);
+    inner();
+    console.log(orderId + 'has been fully processed');
+    };
+}
+
+console.log("Processing orders");
+processOrder(1321, 10.99)();
+processOrder(1322, 12.99)();
+processOrder(1323, 15.0)();
+console.log("All the orders have been processed");
