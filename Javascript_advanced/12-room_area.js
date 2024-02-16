1298ec1f5fad84a2f7038d956284d323049277e5
simonrichard-dev <simon.insa@gmail.com>
2024-02-16 10:21:20 +0100

    add file for task 12. Binding

---
+++ Javascript_advanced/12-room_area.js
@@ -0,0 +1,10 @@
+let roomDimensions = {
+    width: 50,
+    length: 100,
+    getArea() {
+        let area = this.width * this.length;
+        return(area);
+    }
+}
+
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
